f3fab722cd155d080f35e735984cd539e5ad73ca
Anh dv <37327111+TreAt1412@users.noreply.github.com>
2018-11-11 23:23:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/viet/PycharmProjects/App/static/image'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+img = '/home/viet/PycharmProjects/App/static/image/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -28,10 +28,12 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            global img
             filename = secure_filename(file.filename)
+            img = '{}{}'.format(img, filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return redirect(url_for('upload_file',
+                                    filename=upload_file))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -41,4 +43,7 @@ def upload_file():
             <input type=submit value=Upload>
     </form>
     '''
-app.run( debug=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
