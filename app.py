2cb1f58525c68945300a80d484a3cb76395227fe
yeu ha noi <43584342+healer7599@users.noreply.github.com>
2018-11-12 17:38:44 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+import pytesseract, cv2
 
-UPLOAD_FOLDER = '/home/viet/PycharmProjects/App/static/image'
+UPLOAD_FOLDER = '/home/healer/PycharmProjects/hackathon/static/image'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
-img = '/home/viet/PycharmProjects/App/static/image/'
+img = '/home/healer/PycharmProjects/hackathon/static/image/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "Viet Anh"
-
+app.secret_key = "Tuan"
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/show_info', methods=['GET', 'POST'])
 def upload_file():
+    check = 'no'
+    list = []
     if request.method == 'POST':
         # check if the post request has the file part
-        file = request.files['file']
+        file = request.files['file_upload']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file and allowed_file(file.filename):
             global img
-            filename = secure_filename(file.filename)
-            img = '{}{}'.format(img, filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_name = secure_filename(file.filename)
+            img = '{}{}'.format(img, file_name)
+            
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+            check = 'yes'
+            list = ['tuan', '123', '567']
+            link = file_name
+            print(link)
             # ham xu ly anh
-    return '''
-    <!doctype html>
-    <title>Upload Image</title>
-    <h1>Upload images</h1>
-    <form method=post enctype=multipart/form-data>
-        <input type=file name=file>
-            <input type=submit value=Upload>
-            <a href="upload.html"><button>go</button</a>
-    </form>
-    '''
-
-
-@app.route('/')
-def hello():
-    return "hello"
-
+    return render_template('show_info.html', show_more = check, show_info = list, link_img = link)
 
 if __name__ == "__main__":
     app.run(debug=True)
