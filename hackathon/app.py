fb4d2397ca9fca743c57a73e2142c39b1dd71833
Anh dv <37327111+TreAt1412@users.noreply.github.com>
2018-11-18 14:16:21 +0700

    Add files via upload

--- hackathon/app.py
+++ hackathon/app.py
@@ -40,11 +40,6 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     file = request.files['img_upload'] # lay link anh tu user
